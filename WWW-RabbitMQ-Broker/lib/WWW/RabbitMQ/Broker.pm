b8beef30abf879346760a40df625b38b08fd20d2
Shane Utt <sutt@liquidweb.com>
2015-05-28 13:09:39 -0400

    Add shovel to manifest

--- WWW-RabbitMQ-Broker/lib/WWW/RabbitMQ/Broker.pm
+++ WWW-RabbitMQ-Broker/lib/WWW/RabbitMQ/Broker.pm
@@ -3,7 +3,7 @@ package WWW::RabbitMQ::Broker;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use LWP::UserAgent;
 use HTTP::Request;
--- WWW-RabbitMQ-Broker/lib/WWW/RabbitMQ/Broker/Shovel.pm
+++ WWW-RabbitMQ-Broker/lib/WWW/RabbitMQ/Broker/Shovel.pm
@@ -3,7 +3,7 @@ package WWW::RabbitMQ::Broker::Shovel;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub new
 {
